6ee139e64fdfe4c8b8aaba14c2e2c7cefd764c2d
Monteirexx <101936692+Monteirexx@users.noreply.github.com>
2023-01-06 20:02:03 +0000

    Add files via upload

---
+++ creator_blips/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+
+game 'gta5'
+lua54 'yes'
+
+
+--Client Scripts-- 
+client_scripts {
+    'Client/*.lua'
+}
+
+--Server Scripts-- 
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'Server/*.lua'
+}
+
+shared_scripts {
+    'Shared.lua'
+}
+--UI Part-- 
+ui_page {
+    'html/index.html', 
+}
+
+--File Part-- 
+files {
+    'html/index.html',
+    'html/app.js', 
+    'html/style.css'
+} 
